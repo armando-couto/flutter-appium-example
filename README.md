376fc09fce46936a661c68f051495e1c6b697923
Armando Couto <coutoarmando@gmail.com>
2022-11-22 23:35:19 -0300

    Ajustes do exemplo

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 Flutter example project for appium-flutter-driver.
 
+## Node version
+    v16.15.0 (npm v8.5.5)
+
+## Install Mac
+> brew install node      # get node.js
+> npm install -g appium@next  # get appium
+> npm install wd         # get appium client
+> cd appium/ && appium   # start appium
+> flutter build ios
+> cd appium/ && npm run ios
+
+
+## Links
+    https://appium.github.io/appium/docs/en/2.0/guides/caps/
+    https://github.com/appium-userland/appium-flutter-driver
+    https://dev.to/netfirms/flutter-app-testing-with-appium-flutter-driver-33ko
+    https://www.pcloudy.com/automate-flutter-app-using-appium-flutter-driver
--- appium/test.js
+++ appium/test.js
@@ -2,32 +2,39 @@ const wdio = require('webdriverio');
 const assert = require('assert');
 const find = require('appium-flutter-finder');
 
-const osSpecificOps = process.env.APPIUM_OS === 'android' ? {
-  platformName: 'Android',
-  deviceName: 'emulator-5554',
-  // @todo support non-unix style path
-  app: __dirname +  '/../build/app/outputs/apk/debug/app-debug.apk',
-}: process.env.APPIUM_OS === 'ios' ? {
-  platformName: 'iOS',
-  platformVersion: '12.2',
-  deviceName: 'iPhone X',
-  noReset: true,
-  app: __dirname +  '/../ios/Runner.zip',
-
-} : {};
+//const osSpecificOps = process.env.APPIUM_OS === 'android' ? {
+//  platformName: 'Android',
+//  deviceName: 'emulator-5554',
+//  // @todo support non-unix style path
+//  app: __dirname +  '/../build/app/outputs/apk/debug/app-debug.apk',
+//}: process.env.APPIUM_OS === 'ios' ? {
+//
+//  deviceName: 'iPhone ',
+//  noReset: true,
+//  app:
+//} : {};
 
 const opts = {
   port: 4723,
   capabilities: {
-    ...osSpecificOps,
-    automationName: 'Flutter'
+      "platformName": 'iOS',
+      "appium:platformVersion": '16.1',
+      "appium:deviceName": "iPhone X",
+      "appium:app": '/Users/armandocouto/Downloads/flutter-appium-example/build/ios/Debug-iphonesimulator/Runner.app',
+      "appium:automationName": "XCUITest",
+      "appium:noReset": true,
+      "$cloud:appiumOptions": {
+        "appiumVersion": "2.0.0",
+        "automationVersion": "3.52.0",
+        "plugins": ["images"]
+      }
   }
 };
 
 (async () => {
   console.log('Initial app testing')
   const driver = await wdio.remote(opts);
-   assert.strictEqual(await driver.execute('flutter:checkHealth'), 'ok');
+  assert.strictEqual(await driver.execute('flutter:checkHealth'), 'ok');
   await driver.execute('flutter:clearTimeline');
   await driver.execute('flutter:forceGC');
 
